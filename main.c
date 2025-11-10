72fdbbdfa152d406a7f5c882db9c0368d8a70db0
Hawks1299 <mastskel@gmail.com>
2025-11-10 15:18:09 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main() {
+
+    // caracteres
+    char nome_da_variavel = 'a'; // -127, 127
+    unsigned char variavel2; // 0, 255
+    // numeros inteiros
+    short int nome_da_variavel4;
+    int nome_da_variavel2;
+    long int nome_da_variavel5;
+    unsigned short int variavel1;
+    // numeros reais
+    float nome_da_variavel3;
+    double nome_da_variavel7;
+    long double nome_da_variavel8;
+
+}
