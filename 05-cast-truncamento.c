39e1b185fa9cd8f816efc7cb6fe271b58f77ca57
Hawks1299 <mastskel@gmail.com>
2025-11-14 20:29:15 -0400

    chore: Conteudo atualizado da aula de C

---
+++ 05-cast-truncamento.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void main() {
+
+    float b = 3.9;
+    printf("valor convertido para int: %i", (int)b);
+
+}
--- main.c
+++ main.c
@@ -1,38 +1,21 @@
 #include<stdio.h>
 
 void main() {
+    
+    int idade;
+    float altura;
+    char letra_favorita;
 
-    // caracteres
-    char nome_da_variavel = 'a'; // -127, 127
-    unsigned char variavel2; // 0, 255
-    printf("Imprimindo a variavel do tipo CHAR: %c\n", nome_da_variavel);
-    printf("Imprimindo duas variaves do mesmo tipo %c %c\n", nome_da_variavel, variavel2);
+    printf("Qual a sua idade?\n");
+    scanf(" %i", &idade);
+    printf("Qual a sua altura?\n");
+    scanf(" %f", &altura);
+    printf("Qual a sua letra favorita?\n");
+    scanf(" %c", &letra_favorita);
 
-    // numeros inteiros
-    short int nome_da_variavel4;
-    int nome_da_variavel2;
-    long int nome_da_variavel5;
-    printf("short int: %i\n", nome_da_variavel4);
-    printf("int: %i\n", nome_da_variavel2);
-    printf("long ing: %li\n", nome_da_variavel5);
-
-    unsigned short int variavel1;
-    unsigned int variavel3;
-    unsigned long int variavel4;
-    printf("u short int: %u\n", variavel1);
-    printf("u int: %u\n", variavel3);
-    printf("u long int: %lu\n", variavel4);
-
-    // numeros reais
-    float nome_da_variavel3 = 3.9f;
-    double nome_da_variavel7 = 3.4;
-    long double nome_da_variavel8 = 6.9e-23L;
-    printf("float: %f\n", nome_da_variavel3);
-    printf("double: %f\n", nome_da_variavel7);
-    printf("long double: %e\n", nome_da_variavel8);
-
-    // constante nomeada
-    const int MAX = 100; 
-    printf("const: %i\n", MAX);
+    printf("A sua idade e: %i\n"\
+    "A sua altura e: %f\n"\
+    "A sua letra favorita e: %c\n"\
+    , idade, altura, letra_favorita);
 
 }
